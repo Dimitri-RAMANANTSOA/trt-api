6b61ef6f65817edd409f1dd22fa78f57ab900b98
Dimitri-RAMANANTSOA <109164299+Dimitri-RAMANANTSOA@users.noreply.github.com>
2022-10-07 11:57:54 +0200

    init project + JWT auth

---
+++ src/Entity/Annonces.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\AnnoncesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnnoncesRepository::class)]
+#[ApiResource]
+class Annonces
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $place = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $isPublished = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(string $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\User;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Constraints;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    security : "is_granted('ROLE_ADMIN')",
+    normalizationContext : ['groups' => ['user:read']],
+    denormalizationContext : ['groups' => ['user:write']],
+    paginationItemsPerPage : 10,
+    paginationMaximumItemsPerPage : 100,
+    paginationClientItemsPerPage : true,
+    operations: [
+        new Get(),
+        new Post(),
+        new Patch(),
+        new Delete(),
+        new GetCollection(),
+    ]
+)]
+#[UniqueEntity('email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[
+        ORM\Column,
+        Groups(['user:read'])
+    ]
+    private ?int $id = null;
+
+    #[
+        ORM\Column(length: 180, unique: true),
+        Groups(['user:read', 'user:write']),
+        Constraints\NotBlank,
+        Constraints\Email,
+        Constraints\Length(min: 5, max: 100)
+    ]
+    private ?string $email = null;
+
+    #[
+        ORM\Column,
+        Groups(['user:read', 'user:write'])
+    ]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[
+        ORM\Column,
+        Groups(['user:write']),
+        Constraints\NotBlank,
+        Constraints\Length(min: 8, max: 100)
+    ]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $plaintextPassword): self
+    {
+        $this->passwordHasherFactory = new PasswordHasherFactory([
+            // auto hasher with default options for the User class (and children)
+            self::class => ['algorithm' => 'auto']
+        ]);
+
+        $passwordHasher = new UserPasswordHasher($this->passwordHasherFactory);
+
+        // hash the password (based on the password hasher factory config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $this,
+            $plaintextPassword
+        );
+
+        $this->password = $hashedPassword;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
